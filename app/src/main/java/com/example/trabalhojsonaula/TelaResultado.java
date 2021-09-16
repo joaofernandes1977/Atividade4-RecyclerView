c5c31983cf03e875cd3b4465b6a3802d3f9fcdbe
Joao Fernandes <joao.fernandez.hl@gmail.com>
2021-09-16 15:03:24 -0300

    start

--- app/src/main/java/com/example/trabalhojsonaula/TelaResultado.java
+++ app/src/main/java/com/example/trabalhojsonaula/TelaResultado.java
@@ -6,55 +6,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
 
 public class TelaResultado extends AppCompatActivity {
 
 
         private Button chamada1;
-        private Button chamada2;
-        private Button chamada3;
+        //private Button chamada2;
+        //private Button chamada3;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_tela_resultado);
 
             IniciarComponentes();
-
+            chamada1 = findViewById(R.id.todos1);
             chamada1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(TelaResultado.this, TodosActivity.class);
+                    Intent intent = new Intent(TelaResultado.this, GtaActivity.class);
                     startActivity(intent);
                 }
             }
             );
-
-            chamada2.setOnClickListener(new View.OnClickListener() {
+            chamada1 =findViewById(R.id.usuarios);
+            chamada1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(TelaResultado.this, UserActivity.class);
+                    Intent intent = new Intent(TelaResultado.this, MatrixActivity.class);
                     startActivity(intent);
                 }
             } );
-            chamada3.setOnClickListener(new View.OnClickListener() {
+            chamada1 = findViewById(R.id.posts);
+            chamada1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(TelaResultado.this, PostsActivity.class);
+                    Intent intent = new Intent(TelaResultado.this, LoginActivity.class);
                     startActivity(intent);
                 }
             } );
 
         }
         private void IniciarComponentes(){
-            chamada1 = findViewById(R.id.todos);
-            chamada2 = findViewById(R.id.usuarios);
-            chamada3 = findViewById(R.id.posts);
+            //chamada1 = findViewById(R.id.todos);
+            //chamada1 = findViewById(R.id.usuarios);
+            //chamada1 = findViewById(R.id.posts);
         }
 
 
